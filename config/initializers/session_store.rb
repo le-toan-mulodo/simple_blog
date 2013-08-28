43ebc379c45e3d3cfcfb57a29728265194fd7e2a
le-toan-mulodo <le.toan@mulodo.com>
2013-08-28 09:50:54 +0700

    update expired_time

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-<<<<<<< HEAD
+
 DemoRails::Application.config.session_store :cookie_store, key: '_demo_rails_session', :expire_after => 24.hours
-=======
-DemoRails::Application.config.session_store :cookie_store, key: '_demo_rails_session'
->>>>>>> 8a30f1abe215c92c2758540202c0bb690625a5b7
+
